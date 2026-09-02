e04e79f82bdcecaccbfc4898c3c0b7701e738cd1
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2026-09-02 08:46:32 +0000

    Remove duplicate campaign webhook event

--- src/tools/descriptions.ts
+++ src/tools/descriptions.ts
@@ -586,7 +586,6 @@ export const OUTBOUND_WEBHOOK_EVENT_TYPES = [
   "sms.delivered",
   "sms.failed",
   "sms.opted_out",
-  "campaign.sent",
   "subscriber.invalid",
   "subscriber.created",
   "subscriber.updated",
